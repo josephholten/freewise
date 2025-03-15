8ec69d093c09d27f00e2941d24794a1b95e0cda5
Joseph Holten <joseph@holten.com>
2025-03-15 15:26:54 +0100

    bugfix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -124,7 +124,7 @@ const LoginForm = () => {
 
         <div className="text-center mt-4">
           <p className="text-sm text-gray-600">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
               Sign up
             </a>
