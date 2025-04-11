18058de4329f058e091730f7a4d385683f64fd12
Joseph Holten <joseph@holten.com>
2025-04-11 13:53:00 +0200

    simplify middleware

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { decrypt } from "@/app/lib/session";
 import { cookies } from "next/headers";
 
-const secretKey = process.env.JWT_SECRET!;
-const encodedKey = new TextEncoder().encode(secretKey);
-
 export async function middleware(req: NextRequest) {
   const cookieStore = await cookies();
   const session = cookieStore.get("freewise-session")?.value;
